abe8face51d5cd85dfa590b84da098c0cf5087c2
NBluder233 <nbluder@gmail.com>
2018-09-12 13:15:38 -0400

    Adds prompt for password

--- RedditScrape.py
+++ RedditScrape.py
@@ -3,16 +3,27 @@ import praw
 import pandas as pd
 import datetime as dt
 
+passwd = input("Password: ")
+
 reddit = praw.Reddit(client_id='fzr0BShtwiAr_w',
                      client_secret='7WR2TvbreQLYryJtTFSctL-heFU',
                      user_agent='Loota',
                      username='Loota-app',
-                     password='7227kfxx@')
+                     password=passwd)
 
 subNew = reddit.subreddit('Miniswap').new()
 
 def scrape():
-    postDict = {}
+    postDict = {"title": [],
+                "url": [],
+                "created": [],
+                "body": []}
+
+    for submission in subNew:
+        postDict["title"].append(submission.title)
+        postDict["url"].append(submission.url)
+        postDict["created"].append(submission.created)
+        postDict["body"].append(submission.selftext)
 
     return postDict
 
